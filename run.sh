24f2b729d116a9b5b0120e648d715cefd5329f0e
MemoKing34 <karam24@itu.edu.tr>
2026-05-01 02:47:32 +0300

    scripts use `python -m pip` instead of `pip`

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 test -d .venv || python -m venv .venv
-./.venv/bin/pip3 install -U pip -r requirements.txt
+./.venv/bin/python3 -m pip install -U pip -r requirements.txt
 ./.venv/bin/python3 -m ninova_fetcher
