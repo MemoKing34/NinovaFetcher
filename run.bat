13d387003a69209a6bb74653dd9806841e97ac32
MemoKing34 <karam24@itu.edu.tr>
2026-05-01 02:48:23 +0300

    fix typo

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 if not exist .venv (python -m venv .venv)
-.venv\Scripts\python -m install -r requirements.txt -U
+.venv\Scripts\python -m pip install -r requirements.txt -U
 .venv\Scripts\python -m ninova_fetcher
 pause
