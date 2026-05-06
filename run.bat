91b7c45cc413efe7a45795d2bd7d56d075413697
MemoKing34 <karam24@itu.edu.tr>
2026-05-06 19:01:05 +0300

    auto update if git is installed and repo pulled with git

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+where git >nul 2>&1 && if exist .git\NUL git pull
 if not exist .venv (python -m venv .venv)
 .venv\Scripts\python -m pip install -r requirements.txt -U
 .venv\Scripts\python -m ninova_fetcher %*
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+command -v git && file -d .git && git pull
 test -d .venv || python -m venv .venv
 ./.venv/bin/python3 -m pip install -U pip -r requirements.txt
 ./.venv/bin/python3 -m ninova_fetcher $@
