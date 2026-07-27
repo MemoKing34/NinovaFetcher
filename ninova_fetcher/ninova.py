4acf5ae527c5d54eebc3aa3c5546a2dc1defe408
MemoKing34 <karam24@itu.edu.tr>
2026-07-27 23:30:50 +0300

    Fix an error related to not sanitizing filenames properly

--- ninova_fetcher/ninova.py
+++ ninova_fetcher/ninova.py
@@ -266,7 +266,7 @@ class Ninova:
         download_path.mkdir(exist_ok=True)
         title: "element.Tag" = soup.find(attrs={'id': "ctl00_pnlHeader"})
         form2: "element.Tag" = soup.find(attrs={'class': "form2"})
-        (download_path / (title.text.strip() + ".odev.txt")).write_text(form2.text.strip(), encoding='utf-8')
+        (download_path / (sanitize_filename(title.text.strip()) + ".odev.txt")).write_text(form2.text.strip(), encoding='utf-8')
         self.create_link_file(download_path, 'Ödevi Yükle.odev', BASE_URL + _url + '/OdevGonder')
 
         tables: list["element.Tag"] = form2.find_all("table")
