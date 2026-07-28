43d131661b062ee97fd977c6cc3121c5432c5d6c
MemoKing34 <karam24@itu.edu.tr>
2026-07-28 18:34:02 +0300

    use `test` command instead of `file` in `run.sh`

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-command -v git && file -d .git && git pull
+command -v git && test -d .git && git pull
 test -d .venv || python -m venv .venv
 ./.venv/bin/python3 -m pip install -U pip -r requirements.txt
 ./.venv/bin/python3 -m ninova_fetcher $@
